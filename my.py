8c8dde74b70e0eaa428a10039092ce51d0ba6f30
Airborne3 <zemlyanoy98@list.ru>
2025-04-20 12:49:18 +0300

    add my. py

--- my.py
+++ my.py
@@ -1,2 +1,25 @@
 print('Denis')
 print('вторая строка')
+print('привет','мир','пока')
+print(55, 33 , 4.4, True)
+print()
+a=1
+b=2
+print(a+b)
+print('Сумма =',a+b)
+x=2
+y=5
+print(x,'+',y,"=",x+y,)
+print('4','5','6','7',sep='-')
+a=17
+print(a)
+a+=1
+print(a)
+a=a-1
+print(5/2)
+print(5//2)
+print(5*2)
+print(5**2)
+a=4
+b=10
+print('a=',a ,'b=',b)
