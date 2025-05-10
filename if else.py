3db603ac6fee55ec9602bf242c70842c7ca58db9
Airborne3 <zemlyanoy98@list.ru>
2025-05-10 12:51:43 +0300

    add my. py

---
+++ if else.py
@@ -0,0 +1,42 @@
+# a=5
+# b=7
+# if a<b:
+#     print(a)
+#     if a<b:
+#         print('а равно б')
+#     else:
+#         print('а не рвано б'))
+
+#
+# cost = 2000
+# if cost < 1000:
+#     print('cкидки нет')
+# if cost < 3000:
+#     print('скидка 5%')
+# if cost < 5000:
+#     print("скидка 7%")
+# else:
+#     print(' скидка 10%')
+#
+# if cost < 1000:
+#     print('скидки нет')
+#     if cost < 3000:
+#         print('скидка 5%')
+#     else:
+#         print('скидка 10%')
+
+# # задание 1
+# number =6
+# print(number)
+# if number > 5:
+#     print("это число больше пяти")
+
+# задание 2
+a = int(input('Введите целое число '))
+if a > 0:
+    a += 1
+else:
+    a -= 2
+print(a)
+
+# задание 3
